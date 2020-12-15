1f279e8e1c5d05cfa0e77a0f6c0b4249ea859376
45Saurabh45 <75996471+45Saurabh45@users.noreply.github.com>
2020-12-15 15:45:36 +0530

    Add files via upload

---
+++ inheritance/src/inheritance/Inheritance.java
@@ -0,0 +1,7 @@
+package inheritance;
+
+public class Inheritance {
+	int salary=1000;
+	
+
+}
---
+++ inheritance/src/inheritance/Manager.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class Manager extends Inheritance {
+	int id=45;
+	int age=35;
+	
+
+
+public static void main(String[] args) {
+	Manager e=new Manager();
+	System.out.println("employ id=" +e.id + "age="  +e.age + "salary="  +e.salary);
+	
+}
+}
